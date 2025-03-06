e626c4e1365f65f06452d2ee6582eaf001a2bc5c
Rémi Barbier <remi.barbier@hec.edu>
2025-03-06 13:23:32 +0100

    Initial commit du backend

--- routes/share.js
+++ routes/share.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/post');
+const Post = require('../models/Post');
 const User = require('../models/user').UserChoice;
 
 // Sauvegarder un post
