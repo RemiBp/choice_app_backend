a5071babdf81b6840aeb54816b3ac9f107132568
Rémi Barbier <remi.barbier@hec.edu>
2025-03-27 00:07:36 +0100

    MàJ

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -3,18 +3,49 @@ const mongoose = require('mongoose');
 const RestaurantSchema = new mongoose.Schema({
   name: String,
   address: String,
+  coordinates: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: {
+      type: [Number], // [longitude, latitude]
+      required: true
+    }
+  },
   gps_coordinates: {
     type: { type: String, default: "Point" },
     coordinates: { type: [Number], required: true }, // [longitude, latitude]
   },
+  place_id: String,
+  photos: [String],
   rating: Number,
+  ratings: {
+    type: Map,
+    of: Number,
+    default: {}
+  },
+  ratingCounts: {
+    type: Map,
+    of: Number,
+    default: {}
+  },
   price_level: Number,
   category: [String],
   maps_url: String,
-  menus_structures: Object, // Pour stocker les menus
-}, { strict: false }); // Permet d'accepter des champs supplémentaires
+  website: String,
+  phone: String,
+  opening_hours: Object,
+  business_status: String,
+  vicinity: String,
+  description: String,
+  menus_structures: Object, // Pour stocker les structures de menus
+}, { 
+  strict: false // Permet d'accepter des champs supplémentaires
+});
 
-// Ajoute un index géospatial pour les recherches
-RestaurantSchema.index({ gps_coordinates: "2dsphere" });
+// Ajouter un index géospatial pour les recherches
+RestaurantSchema.index({ "gps_coordinates": "2dsphere" });
+RestaurantSchema.index({ "coordinates": "2dsphere" });
 
 module.exports = mongoose.model('Restaurant', RestaurantSchema);
